a9fc80633189a491a8ba4753f9ece02476fe57b3
Igor Rodrigues Alves <81782236+igorrodriguesalves@users.noreply.github.com>
2023-05-31 17:06:29 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,28 @@
 # Importar o módulo csv
 import csv
 
+#Pergunta ao usuário qual letra precisa escolher
+letra = input("Digite uma letra: ")
+#Coloca a letra em maiúsculo
+letra = letra.upper()
+
 # Criar uma lista com os nomes dos 27 arquivos csv
-arquivos = ["TabelaIBPTaxAC23.1.E.csv", "TabelaIBPTaxAL23.1.E.csv", "TabelaIBPTaxAM23.1.E.csv", "TabelaIBPTaxAP23.1.E.csv", "TabelaIBPTaxBA23.1.E.csv", "TabelaIBPTaxCE23.1.E.csv", "TabelaIBPTaxDF23.1.E.csv", "TabelaIBPTaxES23.1.E.csv", "TabelaIBPTaxGO23.1.E.csv", "TabelaIBPTaxMA23.1.E.csv", "TabelaIBPTaxMG23.1.E.csv", "TabelaIBPTaxMS23.1.E.csv", "TabelaIBPTaxMT23.1.E.csv",
-            "TabelaIBPTaxPA23.1.E.csv", "TabelaIBPTaxPB23.1.E.csv", "TabelaIBPTaxPE23.1.E.csv", "TabelaIBPTaxPI23.1.E.csv", "TabelaIBPTaxPR23.1.E.csv", "TabelaIBPTaxRJ23.1.E.csv", "TabelaIBPTaxRN23.1.E.csv", "TabelaIBPTaxRO23.1.E.csv", "TabelaIBPTaxRR23.1.E.csv", "TabelaIBPTaxRS23.1.E.csv", "TabelaIBPTaxSC23.1.E.csv", "TabelaIBPTaxSE23.1.E.csv", "TabelaIBPTaxSP23.1.E.csv", "TabelaIBPTaxTO23.1.E.csv"]
+arquivos =["TabelaIBPTaxAC23.1.F.csv", "TabelaIBPTaxAL23.1.F.csv", "TabelaIBPTaxAM23.1.F.csv", "TabelaIBPTaxAP23.1.F.csv", "TabelaIBPTaxBA23.1.F.csv", "TabelaIBPTaxCE23.1.F.csv", "TabelaIBPTaxDF23.1.F.csv", "TabelaIBPTaxES23.1.F.csv", "TabelaIBPTaxGO23.1.F.csv", "TabelaIBPTaxMA23.1.F.csv", "TabelaIBPTaxMG23.1.F.csv", "TabelaIBPTaxMS23.1.F.csv", "TabelaIBPTaxMT23.1.F.csv",
+            "TabelaIBPTaxPA23.1.F.csv", "TabelaIBPTaxPB23.1.F.csv", "TabelaIBPTaxPE23.1.F.csv", "TabelaIBPTaxPI23.1.F.csv", "TabelaIBPTaxPR23.1.F.csv", "TabelaIBPTaxRJ23.1.F.csv", "TabelaIBPTaxRN23.1.F.csv", "TabelaIBPTaxRO23.1.F.csv", "TabelaIBPTaxRR23.1.F.csv", "TabelaIBPTaxRS23.1.F.csv", "TabelaIBPTaxSC23.1.F.csv", "TabelaIBPTaxSE23.1.F.csv", "TabelaIBPTaxSP23.1.F.csv", "TabelaIBPTaxTO23.1.F.csv"] 
 
 # Criar uma lista vazia para armazenar os valores das colunas
 dados = []
+#Cria outra lista vazia
+nova_lista = []
+#Função que altera a letra do arquivo
+for arquivo in arquivos: # Substituir a letra F pela letra digitada pelo usuário 
+    novo_arquivo = arquivo.replace("F.csv", f"{letra}.csv")
+    # Adicionar o novo arquivo à nova lista 
+    nova_lista.append(novo_arquivo)
 
-# Percorrer cada arquivo da lista de arquivos
-for arquivo in arquivos:
+for arquivo in nova_lista:
     # Abrir o arquivo csv em modo leitura
-    with open(arquivo, "r", encoding='ansi') as f:
+    with open(novo_arquivo, "r", encoding='ansi') as f:
         # Criar um objeto leitor csv
         leitor = csv.DictReader(f, delimiter=';', restval=["codigo", "ex", "nacionalfederal", "importadosfederal", "estadual", "municipal"], dialect='excel')
         # Percorrer cada linha do arquivo csv
@@ -38,31 +49,27 @@ for arquivo in arquivos:
             dados.append(tupla)
 
 # Abrir um arquivo sql em modo escrita
-with open("scriptImportaIBPT.sql", "w") as f:
+with open("scriptImportaIBPL.sql", "w") as f:
     # Percorrer cada tupla da lista de valores
     for tupla in dados:
         # Formatar a tupla como uma string separada por vírgulas
         valores_str = ",".join(tupla)
         # Concatenar a string com o comando SQL de inserção
-        sql = f"INSERT INTO tabela (NCM,IMPOSTO_NACIONAL,IMPOSTO_IMPORTADO,IMPOSTO_ESTADUAL,IMPOSTO_MUNICIPAL, UF, ORIGEM) VALUES ({valores_str});\n"
+        sql = f"INSERT INTO AVANTE_PRINCIPAL.CAD_IMPOSTO_IBPT (NCM,IMPOSTO_NACIONAL,IMPOSTO_IMPORTADO,IMPOSTO_ESTADUAL,IMPOSTO_MUNICIPAL, UF, ORIGEM) VALUES ({valores_str});\n"
         # Escrever o comando SQL no arquivo txt
         f.write(sql)
 
 import os
-#Abre o arquivo
-file = open("scriptImportaIBPT.sql", "r")
-#Verifica a quantidade de linhas e fecha o arquivo
+file = open("scriptImportaIBPL.sql", "r")
 lines = file.readlines()
 file.close()
-#Cria um limite de linhas para dividir o arquivo
 lines_per_file = 120000 
 num_files = 3
 for i in range(num_files):
     new_file = open(f"part_{i+1}.sql", "w")
     new_file.writelines(lines[i*lines_per_file:(i+1)*lines_per_file])
     new_file.close()
-#Apaga o arquivo
-os.remove("scriptImportaIBPT.sql")
-import logging 
-logging.basicConfig(level=logging.WARNING) 
-logging.warning("Script executado com sucesso")
+os.remove("scriptImportaIBPL.sql")
+from tkinter import messagebox
+
+messagebox.showinfo("Script executado com sucesso")
